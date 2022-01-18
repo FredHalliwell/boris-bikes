2466b21768e9ecddc809b67c31a37b431c5cceb8
FredHalliwell <fhalliwell98@gmail.com>
2022-01-18 15:56:58 +0000

    bike and dockingstation classes created

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,6 +1,34 @@
 class DockingStation
+
   def release_bike
-    bike = @bike
-    
-  end 
+    @bike = Bike.new
+  end
+
+  def working?(object)
+    if object.has_wheels
+      return true
+    else   
+      return false
+    end
+  end
 end
+
+class Bike
+
+  def has_wheels
+    return true
+  end
+
+end
+
+
+
+docking_station = DockingStation.new
+
+my_bike = Bike.new
+
+
+
+docking_station.release_bike
+
+puts docking_station.working?(my_bike)
